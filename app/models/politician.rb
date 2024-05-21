0a4b6a08889e6aed192f6005b485e293ac2e55a1
EDUARDO GONZALO <educhigon@gmail.com>
2024-05-21 16:22:25 +0200

    Adding validations to the tables

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -2,4 +2,8 @@ class Politician < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :users, through: :bookings
+
+  validates :name, presence: true, uniqueness: true
+  validates :location, :cost, :description, presence: true
+  validates :cost,  numericality: { only_integer: true }
 end
