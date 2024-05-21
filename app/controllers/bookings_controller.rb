88238303848409e72ff43cd3e111bbaf6b1f9437
Bruno Thormählen <brunothormaehlen@yahoo.com>
2024-05-21 12:18:20 +0200

    seed actually works now

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,9 @@
 class BookingsController < ApplicationController
 
   def my_bookings
+    user_id = 1 # later current_user.id
+    @user = User.all
+    @bookings = Booking.where(user_id: user_id)
   end
 
   def politicians_bookings_index
