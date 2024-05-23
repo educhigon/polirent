7f858b7ef8b477e2b513476db9dff00cc66379f1
Bruno Thormählen <brunothormaehlen@yahoo.com>
2024-05-23 22:25:07 +0200

    added time validation to booking-model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,10 +6,27 @@ class Booking < ApplicationRecord
 
   before_validation :set_default_values
 
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate :no_booking_overlap
+
   private
 
   def set_default_values
     self.status ||= 0
   end
 
+  def no_booking_overlap
+    # errors.add(:base, 'testtest!')
+    overlapping_bookings = Booking.where(politician_id: self.politician_id)
+                                  # .where(status: 1)
+                                  .where.not("
+                                    ( start_date < ? AND end_date < ? ) OR
+                                    ( ? > end_date AND ? > start_date)",
+                                  self.start_date, self.end_date, self.start_date, self.end_date)
+
+    if overlapping_bookings.exists?
+      errors.add(:base, 'overlap!')
+    end
+  end
 end
