ca761e7267a455537c2730f7b41acaaa09eb9bb4
Bruno Thormählen <brunothormaehlen@yahoo.com>
2024-05-24 15:31:08 +0200

    seed slightly improved

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ image_urls = {
   cleopatra: {
     p: "https://www.creativefabrica.com/wp-content/uploads/2022/10/06/Realistic-Portrait-Of-Cleopatra-40463853-1.png"
   },
-sexy_biden_img: {
+  sexy_biden: {
     p: "https://upload.wikimedia.org/wikipedia/commons/6/68/Joe_Biden_presidential_portrait.jpg",
     one: "https://pbs.twimg.com/media/CaL8zrtUsAEJRvJ.jpg:large",
     two: "https://a.abcnews.com/images/Politics/ap_joe_biden_ice_cream_03_jc_141009_4x3_992.jpg"
@@ -33,13 +33,13 @@ sexy_biden_img: {
     p: "https://media.harrypotterfanzone.com/lord-voldemort-deathly-hallows-part-2-portrait.jpg"
   },
   angela_merkel: {
-    p: "https://ygo-assets-entities-us.yougov.net/ef0b5614-b0b7-11e6-adde-09142178952f.jpg?pw=140"
+    p: "https://www.kreiszeitung.de/bilder/2021/12/08/91166227/27658995-altkanzlerin-angela-merkel-cdu-winkt-2XwmSj0Edw6b.jpg"
   },
   barack_obama: {
     p: "https://ygo-assets-entities-us.yougov.net/516e6836-d278-11ea-a709-979a0378f022.jpg?pw=7000"
   },
   dick_cheney: {
-    p: "https://ygo-assets-entities-us.yougov.net/1e08e514-2d03-11e6-9570-cf1c514d3e57.jpg?pw=7000"
+    p: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Richard_Cheney_2005_official_portrait.jpg/800px-Richard_Cheney_2005_official_portrait.jpg"
   },
   donald_trump: {
     p: "https://ygo-assets-entities-us.yougov.net/ddb8156b-fee8-11ed-9d53-6515ccbf4e2f.jpg?zcw=655&zch=655&zct=40&zcl=97&pw=7000"
@@ -74,9 +74,10 @@ descriptions = {
   shinzo_abe: "Shinzo Abe (born September 21, 1954 – died July 8, 2022) was a Japanese politician and statesman who served as Prime Minister of Japan and President of the Liberal Democratic Party (LDP) from 2006 to 2007 and again from 2012 to 2020. He was the longest-serving prime minister in Japanese history, serving for almost nine years in total. Abe also served as Chief Cabinet Secretary from 2005 to 2006 under Junichiro Koizumi and was briefly the opposition leader in 2012.",
   jair_bolsonaro: "Jair Messias Bolsonaro (born March 21, 1955) is a Brazilian politician and retired military officer who served as the 38th president of Brazil from 2019 to 2023. He previously served as member of Brazil's Chamber of Deputies from 1991 to 2018.",
   mohammed_bin_salman: "Mohammed bin Salman Al Saud (born August 31, 1985) is the de facto ruler of Saudi Arabia. The heir apparent to the Saudi Arabian throne, he is currently Crown Prince and Prime Minister. He is the seventh son of King Salman of Saudi Arabia and grandson of the nation's founder, King Abdulaziz.",
-  bernie_sanders: "Bernard Sanders (born September 8, 1941) is an American politician and activist who is the senior United States senator from Vermont. Sanders is the longest-serving independent in U.S. congressional history but has a close relationship with the Democratic Party, having caucused with House and Senate Democrats for most of his congressional career and sought the party's presidential nomination in 2016 and 2020, coming second in both campaigns. He is often seen as a leader of the U.S. progressive movement."
+  bernie_sanders: "Bernard Sanders (born September 8, 1941) is an American politician and activist who is the senior United States senator from Vermont. Sanders is the longest-serving independent in U.S. congressional history but has a close relationship with the Democratic Party, having caucused with House and Senate Democrats for most of his congressional career and sought the party's presidential nomination in 2016 and 2020, coming second in both campaigns. He is often seen as a leader of the U.S. progressive movement.",
+  sexy_biden: "Sexy Biden is a fictional and playful representation of Joe Biden, emphasizing a charming and attractive persona. Known for his stylish appearance and charismatic presence, Sexy Biden is depicted as a confident leader with a sense of humor, often engaging in lighthearted and fun activities. This version of Biden enjoys connecting with people and is celebrated for his approachable and relatable demeanor.",
+  confused_biden: "Confused Biden is a humorous and exaggerated portrayal of Joe Biden, focusing on moments of apparent confusion or forgetfulness. This caricature emphasizes the more human and imperfect side of a political figure, often depicted in scenarios where he is trying to make sense of complex situations. Confused Biden brings a sense of comic relief and relatability, highlighting the challenges and pressures of political life."
 }
-
 
 additional_values = {
   angela_merkel: {
@@ -97,6 +98,16 @@ additional_values = {
   donald_trump: {
     location: 'USA',
     cost: 600,
+    user_id: users.sample.id
+  },
+  sexy_biden: {
+    location: 'USA',
+    cost: 800,
+    user_id: users.sample.id
+  },
+  confused_biden: {
+    location: 'USA',
+    cost: 150,
     user_id: users.sample.id
   },
   xi_jinping: {
@@ -149,19 +160,21 @@ additional_values = {
 tags = ["sexy", "corrupt", "left", "center", "right"]
 
 politicians_data = [
-  { name: 'Angela Merkel', tags: tags.sample, **additional_values[:angela_merkel], description: descriptions[:angela_merkel], image_url: image_urls[:angela_merkel][:p] },
   { name: 'Barack Obama', tags: tags.sample, **additional_values[:barack_obama], description: descriptions[:barack_obama], image_url: image_urls[:barack_obama][:p] },
-  { name: 'Dick Cheney', tags: tags.sample, **additional_values[:dick_cheney], description: descriptions[:dick_cheney], image_url: image_urls[:dick_cheney][:p] },
+  { name: 'Angela Merkel', tags: tags.sample, **additional_values[:angela_merkel], description: descriptions[:angela_merkel], image_url: image_urls[:angela_merkel][:p] },
+  { name: 'Cleopatra', tags: tags.sample, **additional_values[:cleopatra], description: descriptions[:cleopatra], image_url: image_urls[:cleopatra][:p] },
   { name: 'Donald Trump', tags: tags.sample, **additional_values[:donald_trump], description: descriptions[:donald_trump], image_url: image_urls[:donald_trump][:p] },
+  { name: 'Sexy Biden', tags: tags.sample, **additional_values[:sexy_biden], description: descriptions[:sexy_biden], image_url: image_urls[:sexy_biden][:p] },
+  { name: 'Confused Biden', tags: tags.sample, **additional_values[:confused_biden], description: descriptions[:confused_biden], image_url: image_urls[:confused_biden][:p] },
   { name: 'Xi Jinping', tags: tags.sample, **additional_values[:xi_jinping], description: descriptions[:xi_jinping], image_url: image_urls[:xi_jinping][:p] },
+  { name: 'Napoleon', tags: tags.sample, **additional_values[:napoleon], description: descriptions[:napoleon], image_url: image_urls[:napoleon_img][:p] },
   { name: 'Shinzo Abe', tags: tags.sample, **additional_values[:shinzo_abe], description: descriptions[:shinzo_abe], image_url: image_urls[:shinzo_abe][:p] },
+  { name: 'Dick Cheney', tags: tags.sample, **additional_values[:dick_cheney], description: descriptions[:dick_cheney], image_url: image_urls[:dick_cheney][:p] },
+  { name: 'Voldemort', tags: tags.sample, **additional_values[:voldemort], description: descriptions[:voldemort], image_url: image_urls[:voldemort][:p] },
   { name: 'Jair Bolsonaro', tags: tags.sample, **additional_values[:jair_bolsonaro], description: descriptions[:jair_bolsonaro], image_url: image_urls[:jair_bolsonaro][:p] },
   { name: 'Mohammed bin Salman', tags: tags.sample, **additional_values[:mohammed_bin_salman], description: descriptions[:mohammed_bin_salman], image_url: image_urls[:mohammed_bin_salman][:p] },
-  { name: 'Bernie Sanders', tags: tags.sample, **additional_values[:bernie_sanders], description: descriptions[:bernie_sanders], image_url: image_urls[:bernie_sanders][:p] },
   { name: 'Alexander the Great', tags: tags.sample, **additional_values[:alexander_the_great], description: descriptions[:alexander_the_great], image_url: image_urls[:alexander_the_great][:p] },
-  { name: 'Cleopatra', tags: tags.sample, **additional_values[:cleopatra], description: descriptions[:cleopatra], image_url: image_urls[:cleopatra][:p] },
-  { name: 'Voldemort', tags: tags.sample, **additional_values[:voldemort], description: descriptions[:voldemort], image_url: image_urls[:voldemort][:p] },
-  { name: 'Napoleon', tags: tags.sample, **additional_values[:napoleon], description: descriptions[:napoleon], image_url: image_urls[:napoleon_img][:p] }
+  { name: 'Bernie Sanders', tags: tags.sample, **additional_values[:bernie_sanders], description: descriptions[:bernie_sanders], image_url: image_urls[:bernie_sanders][:p] }
 ]
 
 politicians = politicians_data.map { |politician_data| Politician.create!(politician_data) }
