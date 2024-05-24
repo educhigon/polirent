04d8d282a2e8f3c2757788a063f859fb59047f5f
Bruno Thormählen <brunothormaehlen@yahoo.com>
2024-05-24 14:48:38 +0200

    new seed file ; model attribute adjustments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_23_153734) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_24_111503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,6 +73,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_23_153734) do
     t.datetime "updated_at", null: false
     t.string "image_url"
     t.string "tags"
+    t.string "tag"
     t.index ["user_id"], name: "index_politicians_on_user_id"
   end
 
@@ -84,6 +85,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_23_153734) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
