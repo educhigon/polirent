310081dba8b7956bdb9c44de41a65203e2277dae
Bruno Thormählen <brunothormaehlen@yahoo.com>
2024-05-24 11:51:26 +0200

    fixing book button ; trying modal

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_05_23_153734) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,14 +76,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_23_153734) do
     t.index ["user_id"], name: "index_politicians_on_user_id"
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.bigint "politician_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["politician_id"], name: "index_tags_on_politician_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -103,5 +93,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_23_153734) do
   add_foreign_key "bookings", "politicians"
   add_foreign_key "bookings", "users"
   add_foreign_key "politicians", "users"
-  add_foreign_key "tags", "politicians"
 end
